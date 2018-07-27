7ac4de894539a67385bef35c87fdc1ea15789878
pavankalyan99 <41770254+pavankalyan99@users.noreply.github.com>
2018-07-27 12:07:15 +0530

    Update p1.java

--- guvi/p1.java
+++ guvi/p1.java
@@ -12,12 +12,13 @@ class Ideone
 	
         Scanner s= new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
         int num = s.nextInt();
 
         if(num % 2 == 0)
-            System.out.println(num + " is Even");
-        else
-            System.out.println(num + " is Odd");	// your code goes here
+            System.out.println("Even");
+        else if(num%2!=0)
+            System.out.println("Odd");
+		else
+			System.out.println("Zero");// your code goes here
 	}
 }
